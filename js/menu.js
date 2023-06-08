36772242042d1d34c392c77817017c114925b328
JG-Stadler <joaogbstadler@gmail.com>
2023-06-08 14:16:08 -0300

    header e mais atualizados

---
+++ js/menu.js
@@ -0,0 +1,5 @@
+function ativarMenu() {
+    document.getElementById("nav").classList.toggle("nav-ativo");
+    document.getElementById("botao-menu").classList.toggle("menu-ativo");
+    document.getElementById("bg").classList.toggle("bg-ativo")
+}
